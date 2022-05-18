c9d5fdf7003653434f90b1b6c49f737e5d619cc5
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-05-18 22:54:37 +0200

    Init

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import Home from "./pages/Home.svelte";
+</script>
+
+<main>
+  <div class="titlebar" />
+
+  <Home />
+</main>
+
+<style lang="scss" global>
+  @import './style/fonts.css';
+  @import './style/button.scss';
+
+  :root {
+    font-family: Josefin, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+
+    --background: #1a1a1a;
+    --background-border: #242424;
+    --middleground: #202020;
+    --middleground-border: #292929;
+    --foreground: #2b2b2b;
+    --foreground-border: #363636;
+
+    --logo: #ffffff;
+    --highlight: #ececec;
+    --text: #d2c2c3;
+    --lowlight: #525252;
+
+    --green: #7ce38b;
+    --red: #fa7970;
+    --orange: #faa356;
+    --blue: #77bdfb;
+    --purple: #cea5fb;
+  }
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+  }
+
+  main {
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+
+    background-color: var(--middleground);
+  }
+
+  .titlebar {
+    height: 48px;
+    width: 100%;
+
+    background-color: var(--background);
+    border-top: 2px solid var(--background-border);
+  }
+</style>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,98 @@
+<script>
+</script>
+
+<div class="home">
+  <div class="chat">
+    <div class="log">
+
+    </div>
+    <div class="input">
+      <input type="text" placeholder="Message your peers">
+    </div>
+  </div>
+
+  <div class="network">
+    <div class="user" />
+    <button class="button">disabled</button>
+    <button class="button succes">join</button>
+    <button class="button warning">leave</button>
+    <button class="button link">info</button>
+  </div>
+</div>
+
+<style lang="scss">
+  .home {
+    display: flex;
+    width: 100%;
+    height: 100%;
+
+    .chat {
+      width: 200px;
+      height: calc(100% - 2px);
+      display: flex;
+      flex-direction: column;
+
+      background-color: var(--foreground);
+      border-right: 2px solid var(--foreground-border);
+      border-top: 2px solid var(--foreground-border);
+
+      .log {
+        flex-grow: 1;
+        width: 100%;
+      }
+
+      .input {
+        width: 100%;
+        height: 48px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        input {
+          width: calc(100% - 40px);
+          height: calc(100% - 20px);
+          padding: 0 10px 0 10px;
+
+          color: var(--text);
+          background-color: var(--foreground);
+          border: 2px solid var(--foreground-border);
+          border-radius: 8px;
+          outline: none;
+
+          &:focus {
+            border-color: var(--lowlight);
+          }
+
+          &::placeholder {
+            color: var(--lowlight);
+          }
+        }
+      }
+    }
+
+    .network {
+      flex-grow: 1;
+      flex-wrap: wrap;
+      display: flex;
+      max-height: calc(100% - 2px);
+      height: fit-content;
+
+      background-color: var(--middleground);
+      border-top: 2px solid var(--middleground-border);
+      overflow: hidden;
+      font-family: Josefin;
+
+      .user {
+        margin-left: 20px;
+        margin-top: 20px;
+
+        height: 60px;
+        width: 60px;
+
+        background-color: var(--foreground);
+        border: 2px solid var(--foreground-border);
+        border-radius: 10px;
+      }
+    }
+  }
+</style>
