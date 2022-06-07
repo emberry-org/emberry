72a6fef224d82670c6cfa2b367ab578efe7efd8c
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-06-07 21:53:30 +0200

    Removed three duplicate commands

--- src/core/cmd-center/Archive.ts
+++ src/core/cmd-center/Archive.ts
@@ -4,7 +4,4 @@ import { CmdType } from "./Cmd";
 /** Collection of all the executable commands */
 export const CmdArchive: Cmd[] = [
   { title: 'Toggle Developer Tools', type: CmdType.Developer, accelerator: [ 'CmdOrCtrl', 'Shift', 'I' ], action: 'toggle_devtools' },
-  { title: 'Toggle Developer Tools', type: CmdType.Developer, accelerator: [ 'CmdOrCtrl', 'Shift', 'I' ], action: 'toggle_devtools' },
-  { title: 'Toggle Developer Tools', type: CmdType.Developer, accelerator: [ 'CmdOrCtrl', 'Shift', 'I' ], action: 'toggle_devtools' },
-  { title: 'Toggle Developer Tools', type: CmdType.Developer, accelerator: [ 'CmdOrCtrl', 'Shift', 'I' ], action: 'toggle_devtools' }
 ];
