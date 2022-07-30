d2826a68c56ac293227dfb26919776e895f7eed8
devensiv <devensiv@gmail.com>
2022-07-30 13:31:12 +0200

    remove redundant code

--- src-tauri/src/network/ctrl_chnl/requests.rs
+++ src-tauri/src/network/ctrl_chnl/requests.rs
@@ -5,6 +5,8 @@ use smoke::{messages::EmbMessage, User};
 use crate::network::ctrl_chnl::{EmberryMessage, RhizomeConnection};
 use crate::network::Networking;
 
+use super::state;
+
 #[tauri::command(async)]
 pub async fn request_room(
   usr: User,
@@ -17,25 +19,8 @@ pub async fn request_room(
     return Ok(());
   }
 
-  let msg = EmberryMessage::Direct(EmbMessage::Room(usr));
+  let msg = EmbMessage::Room(usr);
 
-  // obtain the sender to the ctrl channel
-  let guard = rc.read().await;
-  let tx = match &*guard {
-    Some(rc) => &rc.channel,
-    None => {
-      return Err(tauri::Error::Io(Error::new(
-        ErrorKind::Other,
-        "No connection to rhizome",
-      )))
-    }
-  };
-
-  // send to ctrl channel
-  tx.send(msg).await.map_err(|_| {
-    tauri::Error::Io(Error::new(
-      ErrorKind::ConnectionReset,
-      "Rhizome connection closed",
-    ))
-  })
+  state::send(&rc, msg).await?;
+  Ok(())
 }
