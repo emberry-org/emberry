725d04377872f299d4e4a668c3d9ab666788b2e0
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-08-28 16:54:05 +0200

    Vite Manual Chunks

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,25 @@ export default defineConfig({
     hmr: { overlay: false },
     port: 3000
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            if (id.includes("@tauri-apps")) {
+              return "tauri";
+
+            // Code Mirror & Lezer are very large // Code Mirror requires style-mod
+            } else if (id.includes("codemirror") || id.includes("style-mod")) {
+              return "codemirror";
+            } else if (id.includes("@lezer")) {
+              return "lezer"
+            }
+        
+            return "vendor"; // all other package goes here
+          }
+        }
+      }
+    }
+  }
 })
