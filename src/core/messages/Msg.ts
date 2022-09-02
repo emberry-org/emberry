5a1fd1bd8db9210cc8594b1bae04bf9ccef34252
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-09-02 21:44:36 +0200

    Message data structure

--- src/core/messages/Msg.ts
+++ src/core/messages/Msg.ts
@@ -1,5 +1,32 @@
-export default interface Msg {
+import type { Remoji } from "./Remoji";
+
+/**
+ * Base template for a chat message.
+ */
+export interface Msg {
+  /** The sender of this message. */
   sender: String,
+  /** The content of this message. */
   content: String,
+  /** The time at which this message was send. */
   time: String,
+  
+  /** Reaction emojis. ``(undefined if there are no emoji reactions)`` */
+  remojis?: Array<Remoji> | undefined,
+
+  /** The target message of this message. ``(undefined if not a reaction)`` */
+  target?: Msg | undefined,
+
+  /** Whether this message is a ping for the local user. ``(undefined if false)`` */
+  ping?: Boolean | undefined,
+}
+
+/**
+ * Extension for thread messages.
+ */
+export interface ThreadMsg extends Msg {
+  /** The messages send in this thread. */
+  messages: Array<Msg>,
+
+  /* NOTE: The title of the thread is stored within the ``Msg -> content`` */
 }
---
+++ src/core/messages/Remoji.ts
@@ -0,0 +1,9 @@
+/**
+ * A reaction emoji. ``(Remoji)``
+ */
+export interface Remoji {
+  /** The char code of the twemoji. */
+  charCode: String,
+  /** The number of users who reacted with this emoji. */
+  quantity: Number,
+}
