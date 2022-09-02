a2a6d785bdc9d387d93be8e9edc11e7d7661b53e
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-09-02 21:45:06 +0200

    Chat history example for testing

--- src/lib/chat/Chat.svelte
+++ src/lib/chat/Chat.svelte
@@ -1,15 +1,44 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import type { Msg, ThreadMsg } from "@core/messages/Msg";
+  //import { onMount } from "svelte";
   import MessageItem from "./MessageItem.svelte";
   import SmartList from "./SmartList.svelte";
 
-  let items = [];
+  let items: Array<Msg> = [
+    {
+      sender: "Mjex",
+      content: "@a Gaming?",
+      time: "Today at 21:28",
+    },
+    {
+      sender: "Mjex",
+      content: "Let's play some Noita,\nif you guys are up for it?",
+      time: "Today at 21:29",
+    },
+    <ThreadMsg> {
+      sender: "Roboolet",
+      content: "Let's go!",
+      time: "Today at 21:32",
+      messages: [],
+    },
+    {
+      sender: "Devensiv",
+      content: "Sure let's go!",
+      time: "Today at 21:34",
+      target: {
+        sender: "Mjex",
+        content: "@a Gaming?",
+        time: "Today at 21:28",
+      },
+      remojis: [{ charCode: '😎', quantity: 1 }, { charCode: '👍️', quantity: 3 }],
+    },
+  ];
 
-  onMount(() => {
-    for (let i = 0; i < 100; i++) {
-      items.push({ sender: 'User', content: i + ' Hello world!' });
-    }
-  });
+  // onMount(() => {
+  //   for (let i = 0; i < 100; i++) {
+  //     items.push({ sender: 'User', content: i + ' Hello world!' });
+  //   }
+  // });
 
 </script>
 
