7739947254544cc16fc76eb13f2c3078f3ef532f
devensiv <devensiv@gmail.com>
2022-10-23 17:04:39 +0200

    feat: impl from for UserRelation

--- src-tauri/src/data/usr_info.rs
+++ src-tauri/src/data/usr_info.rs
@@ -1,10 +1,22 @@
-pub struct UserInfo{
+pub struct UserInfo {
   pub username: String,
   pub relation: UserRelation,
 }
 
-#[derive(Clone, Copy)]
-pub enum UserRelation{
-  Friend,
-  Known,
+#[derive(Clone, Copy, PartialEq)]
+#[repr(u8)]
+pub enum UserRelation {
+  Known = 0,
+  Friend = 1,
+  Undefined,
+}
+
+impl From<u8> for UserRelation {
+  fn from(value: u8) -> Self {
+    match value {
+      0 => Self::Known,
+      1 => Self::Friend,
+      _ => Self::Undefined,
+    }
+  }
 }
