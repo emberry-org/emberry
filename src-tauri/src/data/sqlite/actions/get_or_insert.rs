d0777a974adc0d632aa305e04f0f8ea726c441f8
devensiv <devensiv@gmail.com>
2022-10-23 17:23:08 +0200

    feat: get/insert/update user info from db

--- src-tauri/src/data/sqlite/actions/get_or_insert.rs
+++ src-tauri/src/data/sqlite/actions/get_or_insert.rs
@@ -1,18 +1,68 @@
-use rusqlite::Connection;
-use smoke::{PubKey, User};
+use log::warn;
+use rusqlite::{params, Connection};
+use smoke::User;
 
-use crate::{exec, data::UserInfo};
-
-fn _get_or_insert(db: &mut Connection, data: (&User, UserInfo)) -> Result<User, rusqlite::Error> {
-  let transaction = db.transaction()?;
-  let mut statement = transaction
-    .prepare("SELECT id, username, relation FROM users WHERE tls_cert = (?1)")
-    .unwrap();
-  let rows = statement.query_map([data.0.key], |row| Ok(()));
-  Err(rusqlite::Error::InvalidQuery)
-}
+use crate::{
+  data::{UserInfo, UserRelation},
+  exec,
+};
 
 #[tauri::command]
-pub fn get_or_insert(user: &User, info: UserInfo) -> Result<User, rusqlite::Error> {
-  exec!(_get_or_insert, (user, info))
+pub fn get(user: &User) -> Result<UserInfo, rusqlite::Error> {
+  exec!(_get, user)
+}
+
+fn _get(db: &mut Connection, data: &User) -> Result<UserInfo, rusqlite::Error> {
+  let mut statement =
+    db.prepare("SELECT id, username, relation FROM users WHERE tls_cert = (?1)")?;
+  let certificate = bs58::encode(data.key).into_string();
+  let mut rows = statement.query_map([&certificate], |row| {
+    let id: u64 = row.get(0)?;
+    let username: String = row.get(1)?;
+    let relation = UserRelation::from(row.get::<usize, u8>(2)?);
+    Ok((id, username, relation))
+  })?;
+
+  if let Some(row) = rows.next() {
+    let (_id, name, relation) = row?;
+    if rows.next().is_some() {
+      warn!(
+        "more then one database entry for certifificate: '{}'",
+        &certificate
+      );
+    }
+    Ok(UserInfo {
+      username: name,
+      relation,
+    })
+  } else {
+    Ok(UserInfo {
+      username: certificate,
+      relation: UserRelation::Stranger,
+    })
+  }
+}
+
+fn _update(
+  db: &mut Connection,
+  certificate: String,
+  info: UserInfo,
+) -> Result<(), rusqlite::Error> {
+  let _ = db.execute(
+    "UPDATE users SET username = (?1), relation = (?2) WHERE tls_cert = (?3)",
+    params![info.username, info.relation as u8, certificate],
+  )?;
+  Ok(())
+}
+
+fn _insert(
+  db: &mut Connection,
+  certificate: String,
+  info: UserInfo,
+) -> Result<(), rusqlite::Error> {
+  let _ = db.execute(
+    "INSERT INTO users (username, relation, tls_cert) VALUES (?1, ?2, ?3)",
+    params![info.username, info.relation as u8, certificate],
+  )?;
+  Ok(())
 }
