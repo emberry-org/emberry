9a26cdbf25355e66dec11c8c5382815e53cc9d42
devensiv <devensiv@gmail.com>
2022-10-31 01:13:04 +0100

    feat: load user list from warehouse

--- src/lib/users.svelte
+++ src/lib/users.svelte
@@ -2,76 +2,108 @@
   import { listen } from "@tauri-apps/api/event";
   import { onMount } from "svelte";
   import { UserStatus, storeUser, type User, Leaf } from "./user";
-  import { onItem } from "./store";
+  import { invoke } from "@tauri-apps/api/tauri";
 
   export let users: User[];
 
   onMount(() => {
-    // Subscribe to the on item update event for users.
-    users = JSON.parse(onItem(localStorage, (val) => {
-      users = JSON.parse(val ?? "[]");
-    }, "users") ?? "[]");
+    invoke("get_usrs", { limit: -1, offset: 0 }).then((b_users: any) => {
+      b_users.forEach((user: any) => {
+        let s_user = {
+          key: user.identifier.bs58,
+          name: user.info.username,
+          status: UserStatus.Disconnected,
+        };
+        users.push(s_user);
+      });
+      // update the rendering
+      users = users;
 
-    if (sessionStorage.getItem("has-mounted") === null || sessionStorage.getItem("has-mounted") !== "true") {
-      // Mark all users as disconnected on app load.
-      for (let i = 0; i < users.length; i++) {
-        users[i].status = UserStatus.Disconnected;
-        storeUser(users[i]);
+      //FIXME keep using store user while we dont have a better for status
+      if (
+        sessionStorage.getItem("has-mounted") === null ||
+        sessionStorage.getItem("has-mounted") !== "true"
+      ) {
+        for (let i = 0; i < users.length; i++) {
+          storeUser(users[i]);
+        }
+        sessionStorage.setItem("has-mounted", "true");
       }
-      sessionStorage.setItem("has-mounted", "true");
-    }
+    });
 
-    listen('wants-room', (e: any) => {
+    listen("wants-room", (e: any) => {
       const usrkey = e.payload;
 
-      users = storeUser({ key: usrkey, status: UserStatus.Pending });
+      let s_user = {
+        key: usrkey,
+        name: "Change wants-room to send IdentifiedUserInfo",
+        status: UserStatus.Pending,
+      };
+
+      const userIndex = users.findIndex((u) => u.key === usrkey);
+      // If this user is new then just push them into the array.
+      if (userIndex === -1) {
+        users.push(s_user);
+        users = users; // Update the rendering
+      }
+      // If this user is already present then update their data.
+      else users[userIndex] = s_user;
+      //FIXME keep using store user while we dont have a better for status
+      storeUser({ key: usrkey, status: UserStatus.Pending });
     });
 
     listen("new-room", (e: any) => {
-      users = storeUser({ key: e.payload.peer_id, status: UserStatus.Connected });
+      const usrkey = e.payload.peer_id;
+      let s_user = {
+        key: usrkey,
+        name: "Change new-room to send IdentifiedUserInfo",
+        status: UserStatus.Pending,
+      };
+
+      const userIndex = users.findIndex((u) => u.key === usrkey);
+      if (userIndex === -1)
+        console.error("new-room event but the user was not found in users");
+      // If this user is already present then update their data.
+      else users[userIndex] = s_user; // index assignment updated the rendering
+
+      //FIXME keep using store user while we dont have a better for status
+      storeUser({
+        key: e.payload.peer_id,
+        status: UserStatus.Connected,
+      });
     });
   });
-
 </script>
 
-
-<div class="tools">
-
-</div>
+<div class="tools" />
 
 <ol class="users">
   {#each users as user}
-  
-  <Leaf {user} />
-
+    <Leaf {user} />
   {/each}
 </ol>
 
-
 <style lang="scss">
+  .tools {
+    width: 100%;
+    height: 60px;
+    min-height: 60px;
 
-.tools {
-  width: 100%;
-  height: 60px;
-  min-height: 60px;
+    margin: 0 0 12px 0;
+    background-color: #ffffff08;
+    border-radius: 10px;
+  }
 
-  margin: 0 0 12px 0;
-  background-color: #ffffff08;
-  border-radius: 10px;
-}
+  .users {
+    width: 100%;
+    height: 100%;
 
-.users {
-  width: 100%;
-  height: 100%;
+    margin: 0;
+    padding: 8px 0 0 0;
 
-  margin: 0;
-  padding: 8px 0 0 0;
+    list-style: none;
 
-  list-style: none;
-
-  background-color: #2C2C2C;
-  border-radius: 12px;
-}
-
+    background-color: #2c2c2c;
+    border-radius: 12px;
+  }
 </style>
-
