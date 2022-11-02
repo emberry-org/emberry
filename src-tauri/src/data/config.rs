28df62ddf7b269b59c6fcb8574c80cd6583989e1
devensiv <devensiv@gmail.com>
2022-11-02 11:02:41 +0100

    feat: lazy static local id

--- src-tauri/src/data/config.rs
+++ src-tauri/src/data/config.rs
@@ -1,6 +1,25 @@
-use super::{path::CONFIG, PemfileReader};
+use crate::data::UserIdentifier;
 
-pub fn pem_reader() -> PemfileReader {
+use super::{path::CONFIG, PemfileReader};
+use lazy_static::lazy_static;
+
+lazy_static! {
+  pub static ref PEM: PemfileReader = pem_reader();
+  pub static ref ID: Option<UserIdentifier<'static>> = {
+    if !PEM.filepath.exists() {
+      return None;
+    }
+    match (&*PEM).try_into() {
+      Ok(id) => Some(id),
+      Err(err) => {
+        log::error!("Unable to obtain local user id: '{}'", err);
+        None
+      }
+    }
+  };
+}
+
+fn pem_reader() -> PemfileReader {
   let mut filepath = CONFIG.clone();
   filepath.push("identity.pem");
   PemfileReader { filepath }
