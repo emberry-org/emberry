97ff2eea5921b533d8245f4cf52b294c0d63f167
devensiv <devensiv@gmail.com>
2022-11-08 10:20:44 +0100

    feat: usr_name_ event for local username change

--- src-tauri/src/data/tauri/mod.rs
+++ src-tauri/src/data/tauri/mod.rs
@@ -1,6 +1,8 @@
 use std::borrow::Cow;
 use std::io::ErrorKind;
 
+use tauri::Window;
+
 use super::sqlite::user_batch::get_limit_offset;
 use super::{config, IdentifiedUserInfo, UserIdentifier, UserInfo};
 
@@ -30,14 +32,22 @@ pub fn get_local<'a>() -> Option<IdentifiedUserInfo<'a>> {
 }
 
 #[tauri::command]
-pub fn update_username(name: String) {
+pub fn update_username(window: Window, name: String) {
+  let frontend_event = |info: &IdentifiedUserInfo| {
+    let event = format!("usr_name_{}", info.identifier.bs58);
+    if let Err(err) = window.emit(&event, &info.info.username) {
+      log::error!("Failed to emit event: '{}'", err);
+    }
+  };
   let mut lock = config::IDI.write().unwrap();
   let option = lock.as_mut();
   if let Some(mut id_info) = option {
-    id_info.info.username = name;
-    match exec(upsert, (id_info, |_| ())) {
-      Ok(()) => (),
-      Err(err) => log::warn!("Could not update local username: '{}'", err),
+    if name != id_info.info.username {
+      id_info.info.username = name;
+      match exec(upsert, (id_info, frontend_event)) {
+        Ok(()) => (),
+        Err(err) => log::warn!("Could not update local username: '{}'", err),
+      }
     }
   }
 }
