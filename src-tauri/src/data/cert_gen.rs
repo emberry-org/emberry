d7c51db1e1ab998da393c12a9652279be8744001
devensiv <devensiv@gmail.com>
2022-12-15 19:23:27 +0100

    fix: remove misplaced check
    fixes #28

--- src-tauri/src/data/cert_gen.rs
+++ src-tauri/src/data/cert_gen.rs
@@ -16,10 +16,6 @@ pub fn generate_cert(pemfile: &PathBuf) -> io::Result<()> {
   let subject_alt_names = vec!["embery_user".to_string()];
   let cert = generate_simple_self_signed(subject_alt_names).unwrap();
 
-  if !pemfile.is_file() {
-    return Err(io::Error::new(std::io::ErrorKind::InvalidInput, "Cannot create pemfile without filename"));
-  }
-
   // since every path that is a filepath has at least "/" as parent unchecked unwrap is ok here
   let dir = unsafe { pemfile.parent().unwrap_unchecked() };
   DirBuilder::new().recursive(true).create(dir)?;
