2c56443f4c8ddd035f0ec41442fc4ac389de40bd
devensiv <devensiv@gmail.com>
2023-03-25 09:09:46 +0100

    feat: multishot vlan

--- src-tauri/src/network/p2p_tunl/vlan.rs
+++ src-tauri/src/network/p2p_tunl/vlan.rs
@@ -9,17 +9,19 @@ use log::trace;
 
 /// The part connecting to the game client
 pub async fn listen(local_port: u16, mut remote_rx: Receiver<Vec<u8>>, remote_tx: Sender<Vec<u8>>) {
-  trace!("starting VLAN Listener");
-
   let listener_l = TcpListener::bind(format!("127.0.0.1:{local_port}"))
     .await
     .expect("could not bind vlan socket");
-  let (mut local_trx, _) = listener_l
-    .accept()
-    .await
-    .expect("could not accept connection to vlan");
 
-  spin(&mut local_trx, (&mut remote_rx, &remote_tx)).await;
+  loop {
+    trace!("starting VLAN Listener");
+    let (mut local_trx, _) = listener_l
+      .accept()
+      .await
+      .expect("could not accept connection to vlan");
+
+    spin(&mut local_trx, (&mut remote_rx, &remote_tx)).await;
+  }
 }
 
 /// The part connecting to the game server
@@ -28,22 +30,24 @@ pub async fn connect(
   mut remote_rx: Receiver<Vec<u8>>,
   remote_tx: Sender<Vec<u8>>,
 ) {
-  // wait for data before we even connect the socket (could be extra signal)
-  let data = remote_rx.recv().await.expect("no first msg");
-  trace!("connecting VLAN socket");
+  loop {
+    // wait for data before we even connect the socket (could be extra signal)
+    let data = remote_rx.recv().await.expect("no first msg");
+    trace!("connecting VLAN socket");
 
-  let local_s = TcpSocket::new_v4().expect("could not make socket");
-  let mut local_trx = local_s
-    .connect(format!("127.0.0.1:{local_port}").parse().unwrap())
-    .await
-    .expect("could not connect socket");
+    let local_s = TcpSocket::new_v4().expect("could not make socket");
+    let mut local_trx = local_s
+      .connect(format!("127.0.0.1:{local_port}").parse().unwrap())
+      .await
+      .expect("could not connect socket");
 
-  local_trx
-    .write_all(&data)
-    .await
-    .expect("could not send initial");
+    local_trx
+      .write_all(&data)
+      .await
+      .expect("could not send initial");
 
-  spin(&mut local_trx, (&mut remote_rx, &remote_tx)).await;
+    spin(&mut local_trx, (&mut remote_rx, &remote_tx)).await;
+  }
 }
 
 async fn spin(local_trx: &mut TcpStream, remote_trx: (&mut Receiver<Vec<u8>>, &Sender<Vec<u8>>)) {
@@ -58,14 +62,13 @@ async fn spin(local_trx: &mut TcpStream, remote_trx: (&mut Receiver<Vec<u8>>, &S
         let data_l = Vec::from(&buf[0..amount_l]);
         remote_tx.send(data_l).await.expect("vlan sender fail");
         if amount_l == 0 {
-          trace!("VLAN: closed");
+          trace!("VLAN: closed socket, restarting");
           return;
         }
       }
       Some(data_r) = remote_rx.recv() => {
         if data_r.is_empty() {
-          remote_tx.send(vec![]).await.expect("vlan sender fail");
-          trace!("VLAN: closed");
+          trace!("VLAN: closed remote, restarting");
           return;
         }
         local_tx.write_all(&data_r).await.expect("could not write all remote vlan data");
