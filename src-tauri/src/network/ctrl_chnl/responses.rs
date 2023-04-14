655c6fa644dfda718e208617a41ecdd220b851c5
devensiv <devensiv@gmail.com>
2023-04-14 17:50:35 +0200

    fix: handle timeout in accept room

--- src-tauri/src/network/ctrl_chnl/responses.rs
+++ src-tauri/src/network/ctrl_chnl/responses.rs
@@ -4,6 +4,7 @@ use crate::{
 };
 
 use super::{state, RhizomeConnection};
+use log::warn;
 use smoke::messages::EmbMessage;
 use smoke::User;
 use std::{
@@ -19,23 +20,34 @@ pub async fn accept_room(
   net: tauri::State<'_, Networking>,
   rc: tauri::State<'_, RhizomeConnection>,
 ) -> Result<()> {
-  {
-    let ident = UserIdentifier {
-      bs58: Cow::Borrowed(&bs58cert),
-    };
-    let usr: User = (&ident).try_into()?;
-
-    let mut guard = net.pending.lock().unwrap();
-    let some = if accepted {
-      guard.insert(usr, RRState::Agreement).is_some()
-    } else {
-      guard.contains_key(&usr)
-    };
-    if some {
-      return Err(tauri::Error::Io(Error::new(
+  let ident = UserIdentifier {
+    bs58: Cow::Borrowed(&bs58cert),
+  };
+  let usr: User = (&ident).try_into()?;
+  let err = || {
+    Err(tauri::Error::Io(Error::new(
         ErrorKind::Other,
         "It should not be possible to interact with the room request popup if the request is already pending"
-      )));
+      )))
+  };
+
+  {
+    let mut guard = net.pending.lock().unwrap();
+    match guard.entry(usr) {
+      std::collections::hash_map::Entry::Occupied(mut entry) => match entry.get() {
+        RRState::Pending(timer) => {
+          if timer.elapsed() > smoke::ROOM_REQ_TIMEOUT {
+            entry.insert(RRState::Agreement);
+          } else {
+            warn!("tried accepting room that was already in agreement");
+            return err();
+          }
+        }
+        RRState::Agreement => return err(),
+      },
+      std::collections::hash_map::Entry::Vacant(entry) => {
+        entry.insert(RRState::Agreement);
+      }
     }
   }
 
